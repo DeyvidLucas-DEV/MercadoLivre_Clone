80da7b6a0a3b495e131266bdcc6c63ed3ba1fd71
DeyvidXYZ <deyvidlucas_026@outlook.com>
2023-11-17 19:49:05 -0300

    Preparação

--- src/components/CardBenSection.jsx
+++ src/components/CardBenSection.jsx
@@ -1,18 +1,34 @@
 import CardBeneficios from "./CardsBeneficios";
-import '../css/CardBeneficios.style.css';
-import DisneyStarLogo from '../image/logodisstar.jpg'
+import "../css/CardBeneficios.style.css";
+import DisneyStarLogo from "../image/logodisstar.jpg";
+import Paramount from "../image/logopara.png";
+import Hbo from "../image/logohbo.png";
 
-export default function CardBenSection(props){
-    
-    return(
-        <>
-        <div className="CardSection">
-            <CardBeneficios TituloCards="Sem custos com a Meli+" SubTitleCards="Disney+ e Star+" ImageContainer={DisneyStarLogo} />
-            <CardBeneficios />
-            <CardBeneficios />
-
+export default function CardBenSection(props) {
+  return (
+    <>
+      <div className="CardSection">
+        <div className="DivTitleBen">
+          <p className="TextTitle">Benefícios em conteúdo</p>
         </div>
-        </>
-
-    )
+        <div className="ContentBens">
+          <CardBeneficios
+            TituloCards="Sem custos com a Meli+"
+            SubTitleCards="Disney+ e Star+"
+            ImageContainer={DisneyStarLogo}
+          />
+          <CardBeneficios
+            TituloCards="Até 30% OFF"
+            SubTitleCards="HBO MAX"
+            ImageContainer={Hbo}
+          />
+          <CardBeneficios
+            TituloCards="Até 30% OFF"
+            SubTitleCards="Paramount+"
+            ImageContainer={Paramount}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
